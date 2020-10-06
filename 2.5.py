a4ccc6154e047e0befd8c71339b6ef9fdc9e4339
vladimir-pakhomov20 <72213001+vladimir-pakhomov20@users.noreply.github.com>
2020-10-06 13:42:36 +0300

    Update 2.5.py

--- 2.5.py
+++ 2.5.py
@@ -4,10 +4,14 @@ b = int(input("Введите число: "))
 for i in range(len(a)):
         if a[i] == b:
             a.insert(i + 1, b)
+            break
         elif a[0] < b:
             a.insert(0, b)
+            break
         elif a[-1] > b:
             a.append(b)
+            break
         elif a[i] > b and a[i + 1] < b:
             a.insert(i + 1, b)
+            break
 print(f"Новый рейтинг - {a}")
