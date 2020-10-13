4bda5e889ee0117d4aadfe033e9546de568d19a2
vladimir-pakhomov20 <72213001+vladimir-pakhomov20@users.noreply.github.com>
2020-10-13 22:33:32 +0300

    Update 2.5.py

--- 2.5.py
+++ 2.5.py
@@ -1,17 +1,18 @@
 a = [7, 5, 3, 3, 2]
-print(f"Рейтинг: {a}")
 b = int(input("Введите число: "))
-for i in range(len(a)):
-        if a[i] == b:
-            a.insert(i + 1, b)
-            break
-        elif a[0] < b:
-            a.insert(0, b)
-            break
-        elif a[-1] > b:
-            a.append(b)
-            break
-        elif a[i] > b and a[i + 1] < b:
-            a.insert(i + 1, b)
-            break
-print(f"Новый рейтинг - {a}")
+while True:
+    if a.count(b):
+        a.insert(a.index(b) + a.count(b), float(b))
+    else:
+        param = 0
+        n_param = 0
+        for n, i in enumerate(a):
+            if b > i:
+               if param < i:
+                   param = i
+                   n_param = n
+            else:
+                n_param = n + 1
+        a.insert(n_param, b)
+    print(f"Новый рейтинг - {a}")
+    break
