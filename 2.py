12bde46607864f487882603d3abc145a9e47a62b
vladimir-pakhomov20 <72213001+vladimir-pakhomov20@users.noreply.github.com>
2020-10-19 23:45:01 +0300

    Delete 2.py

--- 2.py
+++
@@ -1,8 +0,0 @@
-a = int (input ("Количество секунд "))
-if ( a >= 356400 ):
-    print ("To much for this code")
-else:
-    h = int ( a / 3600 )
-    m = int (( a - h * 3600 ) / 60)
-    s = int ( a - h * 3600 - m * 60)
-    print (f"{h:02}:{ m:02}:{s:02}")
